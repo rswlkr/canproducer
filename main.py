2c3558567778a04a723a04da6f71a852526d9f38
Ross Walker <ross.walker@costain.com>
2022-02-10 14:59:39 +0000

    more work

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from kafka import KafkaProducer
 from kafka.errors import KafkaError
 import sys
 import can
+import json
+from bson import json_util
+import time
+import random
+import os,binascii
 
 env = sys.argv[1]
 print("env: " + env)
@@ -23,11 +28,26 @@ if env == "linux":
 
 producer = KafkaProducer(bootstrap_servers=[kafkaServer],api_version=(3,0,0))
 
-# print(producer)
-# # Asynchronous by default
-#
-# future = producer.send('cancar-events', b'raw_bytes')
-#
+def getJsonBytes(data):
+    return json.dumps(data,default=json_util.default).encode('utf-8')
+
+def loadTest():
+    while True:
+        data = {
+            'timestamp':int(time.time()),
+            'busTime':int(time.time()) - 2,
+             'id': random.randint(0,50),
+             'data': binascii.b2a_hex(os.urandom(8))
+         }
+        print (data)
+        future = producer.send('cancar-events', getJsonBytes(data) , key=b'23')
+
+loadTest()
+print(producer)
+# Asynchronous by default
+
+
+
 # # Block for 'synchronous' sends
 # try:
 #     record_metadata = future.get(timeout=2)
@@ -41,8 +61,14 @@ from can.interface import Bus
 bus = Bus()
 
 
+
 for msg in bus:
     print(msg)
+    data = {
+        'timestamp':msg.timestamp,
+        'id': msg.arbitration_id,
+        'data': msg.data
+    }
     future = producer.send('cancar-events', key=bytes(str(msg.arbitration_id), encoding='ascii'), value=bytes(msg.data))
 
 # produce keyed messages to enable hashed partitioning
