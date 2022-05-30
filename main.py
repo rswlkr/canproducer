2f6f4e301a7568753e4c70c4df9b5a5851022887
Ross Walker <ross.walker@costain.com>
2022-05-30 12:13:27 +0100

    using confluent kafka

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
+from confluent_kafka import Producer
 import sys
 import can
 import json
@@ -7,12 +6,15 @@ from bson import json_util
 import time
 import random
 import os,binascii
+import asyncio
+from can.interface import Bus
 
+
+clientID = "123"
 env = sys.argv[1]
 print("env: " + env)
-kafkaServer = sys.argv[2]
-print("server: " + kafkaServer)
-ch =sys.argv[3]
+#ec2-52-19-41-28.eu-west-1.compute.amazonaws.com:9092
+ch =sys.argv[2]
 print("can channel: " + ch)
 if env == "mac":
     can.rc['interface'] = 'virtual'
@@ -26,7 +28,41 @@ if env == "linux":
       can.rc['channel'] = channel
       can.rc['bitrate'] = 500000
 
-producer = KafkaProducer(bootstrap_servers=[kafkaServer],api_version=(3,0,0))
+async def main():
+     p = Producer({
+        'bootstrap.servers': 'SERVER',
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanisms':'PLAIN',
+        'sasl.username': 'USERNAME',
+        'sasl.password': 'PASSWORD',
+     })
+     data = {
+        'id': clientID,
+        'can': {
+             'timestamp':int(time.time()),
+             'data': binascii.b2a_hex(os.urandom(8))
+        }
+     }
+     print(data)
+     p.produce(topic='dev', value=getJsonBytes(data), key=clientID)
+     p.flush()
+
+
+
+     bus = Bus()
+    #
+     for msg in bus:
+         print(msg)
+         data = {
+              'id': clientID,
+                 'can': {
+                      'id': msg.arbitration_id,
+                      'timestamp':msg.timestamp,
+                      'data':  msg.data
+                 }
+        }
+#         p.produce(topic='dev', key=clientID, value=msg.data)
+
 
 def getJsonBytes(data):
     return json.dumps(data,default=json_util.default).encode('utf-8')
@@ -40,36 +76,18 @@ def loadTest():
              'data': binascii.b2a_hex(os.urandom(8))
          }
         print (data)
-        future = producer.send('cancar-events', getJsonBytes(data) , key=b'23')
+        future = producer.send('Test', getJsonBytes(data) , key=b'23')
 
-loadTest()
-print(producer)
-# Asynchronous by default
+# def on_send_success(record_metadata):
+#     print("yo")
+#     print(record_metadata.topic)
+#     print(record_metadata.partition)
+#     print(record_metadata.offset)
+#
+# def on_send_error(excp):
+#     log.error('I am an errback', exc_info=excp)
+#     # handle exception
 
-
-
-# # Block for 'synchronous' sends
-# try:
-#     record_metadata = future.get(timeout=2)
-#     print(record_metadata)
-# except KafkaError as err:
-#     # Decide what to do if produce request failed...
-#     print(err)
-#     pass
-
-from can.interface import Bus
-bus = Bus()
-
-
-
-for msg in bus:
-    print(msg)
-    data = {
-        'timestamp':msg.timestamp,
-        'id': msg.arbitration_id,
-        'data': msg.data
-    }
-    future = producer.send('cancar-events', key=bytes(str(msg.arbitration_id), encoding='ascii'), value=bytes(msg.data))
 
 # produce keyed messages to enable hashed partitioning
 # producer.send('cancar-events', key=b'foo', value=b'bar')
@@ -97,3 +115,4 @@ for msg in bus:
 #
 # # block until all async messages are sent
 # producer.flush()
+asyncio.run(main())
